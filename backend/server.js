6755d34df6e6c967fa459bda18d8f718a76eb154
salinaj27 <salinajohn80@gmail.com>
2024-02-23 08:53:23 -0500

    adding routes and setting up db

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log("Server...");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+//routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
