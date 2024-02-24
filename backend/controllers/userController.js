ac1ddac06a410660d5ff3f09075567e2eea0c6e7
salinaj27 <salinajohn80@gmail.com>
2024-02-24 12:02:15 -0500

    added bcrypt to hash user password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const bcyrpt = require("bcrypt");
+
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -9,7 +12,35 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please include all fields");
   }
 
-  res.send("Register Route");
+  //Find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //Hash Password
+  const salt = await bcyrpt.genSalt(10);
+  const hashedPassword = await bcyrpt.hash(password, salt);
+
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new error("Invalid user data");
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
