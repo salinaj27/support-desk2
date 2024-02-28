5127284f01661f52816fc79e910db4c41b7795cc
salinaj27 <salinajohn80@gmail.com>
2024-02-28 17:23:42 -0500

    Added login and register links in Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Support Desk</Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="/login">
+            <FaSignInAlt /> Login
+          </Link>
+          <Link to="/register">
+            <FaUser /> Register
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
